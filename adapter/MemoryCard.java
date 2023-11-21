ea5f35bdff5297c9d4d32568f53b5b2e87b6d740
Idzanaagi <87720620+Idzanaagi@users.noreply.github.com>
2023-11-21 17:19:24 +0300

    Add files via upload

---
+++ adapter/MemoryCard.java
@@ -0,0 +1,12 @@
+package org.example.tests.patterns.adapter;
+
+public class MemoryCard {
+
+    public void insert() {
+        System.out.println("Карта памяти успешно вставлена!");
+    }
+
+    public void copyData() {
+        System.out.println("Данные скопированы на компьютер!");
+    }
+}
---
+++ adapter/USB.java
@@ -0,0 +1,6 @@
+package org.example.tests.patterns.adapter;
+
+public interface USB {
+
+    void connectWithUsbCable();
+}
